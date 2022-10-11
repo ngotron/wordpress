b09f7c87b40d50a075228d27a37eb73b3f074c0a
Ngo Thi Tron <ngotron01.06@gmail.com>
2022-10-11 08:43:35 +0700

    update

--- wp-content/themes/twentytwentytwo/page-homepage.php
+++ wp-content/themes/twentytwentytwo/page-homepage.php
@@ -14,101 +14,105 @@
 </style>
 
 <body>
-    <>
-        <div class="nar_menu">
-            <div class="menu_left">
-                <img src="https://vov.vn/themes/custom/vovvn/images/logo_meta_home.jpg" alt="">
-                <p>Hôm nay là ngày 05/10/2002</p>
-            </div>
+    <div class="nar_menu">
+        <div class="menu_left">
+            <img src="https://vov.vn/themes/custom/vovvn/images/logo_meta_home.jpg" alt="">
+            <p>Hôm nay là ngày 05/10/2002</p>
+        </div>
 
-            <div class="menu_midle">
-                <a href="#"><i class="fa-brands fa-facebook"></i></a>
-                <a href="#"><i class="fa-brands fa-tiktok"></i></a>
-                <a href="#"><i class="fa-brands fa-youtube"></i></a>
-                <a href="#"><i class="fa-brands fa-skype"></i></a>
-                <p>Nghe & xem <i class="fa-solid fa-circle-chevron-right"></i></p>
-
-            </div>
-            <div class="menu_right">
-                <p>English</p>
-                <p><input type="text"><i class="fa-solid fa-magnifying-glass"></i></p>
-            </div>
+        <div class="menu_midle">
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            <a href="#"><i class="fa-brands fa-skype"></i></a>
+            <p>Nghe & xem <i class="fa-solid fa-circle-chevron-right"></i></p>
 
         </div>
-        <div class="navbar_menu">
-            <p><i class="fa-solid fa-house"></i>Chính trị </p>
-            <p>Xã hội</p>
-            <p>Thế giới</p>
-            <p>Kinh tế</p>
-            <p>Thể thao</p>
-            <p>Văn hóa</p>
-            <p>Phát luật</p>
-            <p>Du lịch</p>
-            <p>Quân sự - Quốc phòng</p>
-            <p>Sức khỏe</p>
-            <p>Đời sống</p>
-            <p>Postcast <i class="fa-solid fa-bars"></i></p>
+        <div class="menu_right">
+            <p>English</p>
+            <p><input type="text"><i class="fa-solid fa-magnifying-glass"></i></p>
         </div>
-        <div class="container">
-            <div class="container__left">
-                <img src="https://vov.vn/themes/custom/vovvn/images/logo_meta_home.jpg" alt="">
-                <ul>
-                    <li>We combed our catalog and found courses that we think match your interests. </li>
-                    <li>We combed our catalog and found courses that we think match your interests. </li>
-                    <li>We combed our catalog and found courses that we think match your interests. </li>
-                    <li>We combed our catalog and found courses that we think match your interests. </li>
-                    <li>We combed our catalog and found courses that we think match your interests. </li>
-                    <li>We combed our catalog and found courses that we think match your interests. </li>
-                </ul>
-            </div>
-            <div class="container__right">
-                <div class="container__left--child">
-                    <div class="container__left--child-one">
-                        <img src="https://vov.vn/themes/custom/vovvn/images/logo_meta_home.jpg" alt="">
-                        <h4>Recommendations for you.</h4>
-                        <p>We combed our catalog and found courses that we think match your interests. We combed our
-                            catalog
-                            and found courses that we think match your interests. We combed our catalog and found
-                            courses
-                            that we think match your interests. </p>
-                    </div>
-                    <div class="container__left--child-two">
-                        <h4>Recommendations for you.</h4>
-                        <img src="https://vov.vn/themes/custom/vovvn/images/logo_meta_home.jpg" alt="">
-                        <p>We combed our catalog and found courses that we think match your interests. We combed our
-                            catalog
-                            and found courses that we think match your interests. We combed our catalog and found
-                            courses
-                            that we think match your interests. </p>
-                    </div>
 
-
+    </div>
+    <div class="navbar_menu">
+        <p><i class="fa-solid fa-house"></i>Chính trị </p>
+        <p>Xã hội</p>
+        <p>Thế giới</p>
+        <p>Kinh tế</p>
+        <p>Thể thao</p>
+        <p>Văn hóa</p>
+        <p>Phát luật</p>
+        <p>Du lịch</p>
+        <p>Quân sự - Quốc phòng</p>
+        <p>Sức khỏe</p>
+        <p>Đời sống</p>
+        <p>Postcast <i class="fa-solid fa-bars"></i></p>
+    </div>
+    <div class="container">
+        <div class="container__left">
+            <img src="https://media.vov.vn/sites/default/files/styles/front_large/public/2022-10/nga_tan_cong_ten_lua_ukraine.jpg"
+                alt="">
+            <ul>
+                <li>We combed our catalog and found courses that we think match your interests. </li>
+                <li>We combed our catalog and found courses that we think match your interests. </li>
+                <li>We combed our catalog and found courses that we think match your interests. </li>
+                <li>We combed our catalog and found courses that we think match your interests. </li>
+                <li>We combed our catalog and found courses that we think match your interests. </li>
+                <li>We combed our catalog and found courses that we think match your interests. </li>
+            </ul>
+        </div>
+        <div class="container__right">
+            <div class="container__left--child">
+                <div class="container__left--child-one">
+                    <img src="https://media.vov.vn/sites/default/files/styles/front_large/public/2022-10/giao-tranh-ac-liet-xung-quanh-kiev-nga-va-ukraine-hoa-dam.jpg"
+                        alt="">
+                    <h4>Recommendations for you.</h4>
+                    <p>We combed our catalog and found courses that we think match your interests. We combed our
+                        catalog
+                        and found courses that we think match your interests. We combed our catalog and found
+                        courses
+                        that we think match your interests. </p>
                 </div>
-                <div class="container__right--child">
-                    <div class="container__right--child-one genary ">
-                        <img src="https://vov.vn/themes/custom/vovvn/images/logo_meta_home.jpg" alt="">
-                        <p>We combed our catalog and found courses that we think match your interests. We combed our
-                            catalog
-                            and found courses that we think match your interests. </p>
-                    </div>
-                    <div class="container__right--child-two genary">
-                        <img src="https://vov.vn/themes/custom/vovvn/images/logo_meta_home.jpg" alt="">
-                        <p>We combed our catalog and found courses that we think match your interests. We combed our
-                            catalog
-                            and found courses that we think match your interests. </p>
-                    </div>
-                    <div class="container__right--child-three genary">
-                        <img src="https://vov.vn/themes/custom/vovvn/images/logo_meta_home.jpg" alt="">
-                        <p>We combed our catalog and found courses that we think match your interests. We combed our
-                            catalog
-                            and found courses that we think match your interests. </p>
-                    </div>
-
-
+                <div class="container__left--child-two">
+                    <h4>Recommendations for you.</h4>
+                    <img src="https://media.vov.vn/sites/default/files/styles/front_large/public/2022-10/5563187178137267247a-my-ukrain-7373-7810-1660004086.jpg"
+                        alt="">
+                    <p>We combed our catalog and found courses that we think match your interests. We combed our
+                        catalog
+                        and found courses that we think match your interests. We combed our catalog and found
+                        courses
+                        that we think match your interests. </p>
                 </div>
+
+
+            </div>
+            <div class="container__right--child">
+                <div class="container__right--child-one genary ">
+                    <img src="https://media.vov.vn/sites/default/files/styles/large/public/2022-10/anh_minh_hoa_reuters.jpg"
+                        alt="">
+                    <p>We combed our catalog and found courses that we think match your interests. We combed our
+                        catalog
+                        and found courses that we think match your interests. </p>
+                </div>
+                <div class="container__right--child-two genary">
+                    <img src="https://images.dable.io/ad/thumbnails/legacy-resize/0127100803316/1x/16by10" alt="">
+                    <p>We combed our catalog and found courses that we think match your interests. We combed our
+                        catalog
+                        and found courses that we think match your interests. </p>
+                </div>
+                <div class="container__right--child-three genary">
+                    <img src="https://images.dable.io/thumbnail/vov-media.emitech.vn/250X157/b79/144f31ef810b007f16c041d169c4cda3e7d78.jpeg"
+                        alt="">
+                    <p>We combed our catalog and found courses that we think match your interests. We combed our
+                        catalog
+                        and found courses that we think match your interests. </p>
+                </div>
+
+
             </div>
         </div>
-        </<section>
+    </div>
+    </<section>
 </body>
 
 </html>
